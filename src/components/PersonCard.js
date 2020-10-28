4174ffca6638ef26244aadce4c56f3598bfe2d11
Riley Burns <rileyburns345@gmail.com>
2020-10-28 17:24:06 -0600

    assignment complete

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    IncrementItem = () => {
+        this.setState({age: this.state.age + 1})
+    }
     render() {
         const {firstName, lastName, age, hairColor} = this.props
         return (
             <div>
                 <h2>{firstName} {lastName}</h2>
-                <p>{age}</p>
+                <p>{this.state.age}</p>
                 <p>{hairColor}</p>
+        <button onClick={ ()=> this.IncrementItem()}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
